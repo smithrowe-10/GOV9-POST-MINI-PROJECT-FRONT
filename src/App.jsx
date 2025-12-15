4790000d43c8e76c7637b7aa3b98e3761f874a94
smithrowe-10 <smj0418smj@gmail.com>
2025-12-15 10:26:28 +0900

    20251215

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import AuthRoute from "./routes/AuthRoute";
+
 function App() {
 
-    
-
     return <>
-    
+        <AuthRoute />
     </>
+
 }
 
 export default App;
--- src/routes/AuthRoute.jsx
+++ src/routes/AuthRoute.jsx
@@ -1,10 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+import Login from "../pages/Auth/Login";
+import SignUp from "../pages/Auth/Signup";
+
 function AuthRoute() {
 
 
     
-    return <>
-    
-    </>
+    return <Routes>
+        <Route path="/auth/login" element={<Login/>}/>
+        <Route path="/auth/signup" element={<SignUp/>}/>
+    </Routes>
 
 }
 
